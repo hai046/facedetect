65adc8b5c15203e94c3be539824c4d910af5871e
hai046 <haizhu12345@gmail.com>
2016-08-04 19:25:14 +0800

    add commit

--- src/facedetect.h
+++ src/facedetect.h
@@ -24,11 +24,21 @@
 using namespace std;
 using namespace cv;
 
+/**
+ * 检索图片目录 path    然后根据现有的模型 cascadeName 识别出人脸，并建立正样本的描述文件  格式是 fileName num  x y w h x2 y2 w2 h2 ……【具体定义可以查官方文档】
+ */
 int makeFacedetect(string cascadeName, string path, string saveFilePath);
+
+/**
+ * 检查人脸，如果showImage 则可以显示出来
+ */
 string detectAndDraw(bool showImage, string name, Mat& img,
 		CascadeClassifier& cascade, CascadeClassifier& nestedCascade,
 		double scale, bool tryflip);
 vector<string> scanFile(string path);
 
+/**
+ * 显示识别
+ */
 int readFacedetect(string cascadeName, string inputName);
 
