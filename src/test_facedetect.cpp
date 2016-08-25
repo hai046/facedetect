6152ce0c05a4b08d2d720b53b15a82e8198af605
hai046 <haizhu12345@gmail.com>
2016-08-25 17:50:54 +0800

    update

--- src/test_facedetect.cpp
+++ src/test_facedetect.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
 #include "facedetect.h"
 #include "LBPH_face_recognizer.h"
+#include <cstring>
 
 string cascadeName =
-//		"/data/opencv/waiopencv/opecv_data/cascade.xml";
+//"/data/opencv/waiopencv/opecv_data/cascade.xml";
 		"/Users/haizhu/Documents/tools/opencv-2.4.13/data/haarcascades/haarcascade_frontalface_default.xml";
 //
 int main(int argc, const char** argv) {
@@ -14,10 +14,9 @@ int main(int argc, const char** argv) {
 	train(fn_csv, output_file);
 
 	readFacedetect(cascadeName,
-//			"/Users/haizhu/Downloads/data/opencv/CM_KSQBAOHED8s41TXBPxw.jpeg@200w_200h_75q.jpeg",
-			"/Users/haizhu/Downloads/data/opencv/waiopencv/images/ZdcGtHqm7cwrk_tbnqe1gg.jpeg",
-//			"/Users/haizhu/Downloads/1.jpg",
-			output_file);
+//"/Users/haizhu/Downloads/data/opencv/CM_KSQBAOHED8s41TXBPxw.jpeg@200w_200h_75q.jpeg",
+//"/Users/haizhu/Downloads/data/opencv/waiopencv/images/ZdcGtHqm7cwrk_tbnqe1gg.jpeg",
+			"/Users/haizhu/Downloads/1.jpg", output_file);
 
 	return 0;
 }
